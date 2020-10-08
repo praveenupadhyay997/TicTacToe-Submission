10942eb234d6af67941828322248bf3b5d10e0c1
praveenupadhyay997 <praveenupadhyay997@gmail.com>
2020-10-09 04:00:03 +0530

    [Praveen] Add: Added the file for Assigning the board

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TicTacToeGame
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Tic Tac Toe Game");
+            
+            char[] board = TicTacToeGame.AssignBoard();
+
+        }
+    }
+}
---
+++ TicTacToeGame.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToeGame
+{
+    class TicTacToeGame
+    {
+        public static char[] board = new char[10];
+
+        public static char[] AssignBoard()
+        {
+            for(int i=0; i<board.Length;i++)
+            {
+                board[i] = ' ';
+            }
+            return board;
+        }
+    }
+}
